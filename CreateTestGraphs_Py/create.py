c981940ed308bb3f8411c4f21cd063c2dbecf030
Peter Holt <sc16ph@leeds.ac.uk>
2020-04-04 15:19:08 +0100

    working on automated test sets

--- CreateTestGraphs_Py/create.py
+++ CreateTestGraphs_Py/create.py
@@ -10,8 +10,8 @@ from lxml import etree
 from graphs import createGraph
 
 while True:
-    switch_number = 2
-    crossing_number = 2
+    switch_number = 3
+    crossing_number = 3
     # switch_number = input ("Choose number of switch nodes in set one: ")
     # crossing_number = input ("Choose number of crossing nodes in set one: ")
     # number_differences = input ("Choose the number of differences between set one and set two: ")
@@ -19,20 +19,28 @@ while True:
 
 G = createGraph(int(switch_number), int(crossing_number))
 print(nx.info(G))
-# nx.draw(G, with_labels=True, font_weight='bold')
-# plt.show()
+nx.draw(G, with_labels=True, font_weight='bold')
+plt.show()
 
 root = etree.Element("Connectivity", version="1.0", xmlns="https://www.tracsis.com/infrastructure")
 child1 = etree.SubElement(root, "Nodes")
 child2 = etree.SubElement(root, "TrackSections")
 
 for node in G:
-    etree.SubElement(child1, G.nodes[node]['type'], id=str(node))
+    if G.nodes[node]['type'] == "LimitOfNetwork":
+        etree.SubElement(child1, G.nodes[node]['type'], id=str(node), type="Unknown", track="4")
+    elif G.nodes[node]['type'] == "Switch":
+        in_var = "in"
+        etree.SubElement(child1, G.nodes[node]['type'], id=str(node), in_="track", outMain="f", outBranch="o", branchDirection="Right")
+    elif G.nodes[node]['type'] == "FlatCrossing":
+        etree.SubElement(child1, G.nodes[node]['type'], id=str(node), inMain="f", outMain="d", inCross="f", outCross="g", direction="RighttoLeft")
 
-for edge in range(G.number_of_edges()):
-    
-    etree.SubElement(child2, "TrackSection", id=str(edge))
+edgeID = 1
+for edge in G.edges():
+    etree.SubElement(child2, "TrackSection", id=str(edgeID), length="100", startNode=str(edge[0]), endNode=str(edge[1]))
+    edgeID = edgeID + 1
 
 
+print(G.edges())
 et = etree.ElementTree(root)
 et.write("sample.xml", encoding='utf-8', xml_declaration=True, pretty_print=True)
--- CreateTestGraphs_Py/graphs.py
+++ CreateTestGraphs_Py/graphs.py
@@ -16,7 +16,7 @@ def createGraph( switch, crossing):
         
         random_type = random.choice([0,1])
         if switch_counter == 0:
-            G.add_node(node_number, type="Crossing")
+            G.add_node(node_number, type="FlatCrossing")
             G.add_edge(random.choice(node_list), node_number)
             G.add_edge(random.choice(node_list), node_number)
             G.add_edge(random.choice(node_list), node_number)
@@ -41,7 +41,7 @@ def createGraph( switch, crossing):
             switch_counter -= 1
             node_list.append(node_number)
         elif random_type == 1 and crossing_counter > 0:
-            G.add_node(node_number, type="Crossing")
+            G.add_node(node_number, type="FlatCrossing")
             G.add_edge(random.choice(node_list), node_number)
             G.add_edge(random.choice(node_list), node_number)
             G.add_edge(random.choice(node_list), node_number)
@@ -53,7 +53,7 @@ def createGraph( switch, crossing):
             if G.nodes[node]['type'] == 'Switch' and G.degree[node] == 3:
                 if node in node_list:
                     node_list.remove(node)
-            elif G.nodes[node]['type'] == 'Crossing' and G.degree[node] == 4:
+            elif G.nodes[node]['type'] == 'FlatCrossing' and G.degree[node] == 4:
                 if node in node_list:
                     node_list.remove(node)
         node_number += 1
@@ -72,7 +72,7 @@ def createGraph( switch, crossing):
             G.add_node(add_limit_node_number, type="LimitOfNetwork")
             G.add_edge(node, add_limit_node_number)
             add_limit_node_number += 1
-        elif G.nodes[node]['type'] == 'Crossing' and G.degree[node] == 1:
+        elif G.nodes[node]['type'] == 'FlatCrossing' and G.degree[node] == 1:
             G.add_node(add_limit_node_number, type="LimitOfNetwork")
             G.add_edge(node, add_limit_node_number)
             add_limit_node_number += 1
@@ -82,14 +82,14 @@ def createGraph( switch, crossing):
             G.add_node(add_limit_node_number, type="LimitOfNetwork")
             G.add_edge(node, add_limit_node_number)
             add_limit_node_number += 1
-        elif G.nodes[node]['type'] == 'Crossing' and G.degree[node] == 2:
+        elif G.nodes[node]['type'] == 'FlatCrossing' and G.degree[node] == 2:
             G.add_node(add_limit_node_number, type="LimitOfNetwork")
             G.add_edge(node, add_limit_node_number)
             add_limit_node_number += 1
             G.add_node(add_limit_node_number, type="LimitOfNetwork")
             G.add_edge(node, add_limit_node_number)
             add_limit_node_number += 1
-        elif G.nodes[node]['type'] == 'Crossing' and G.degree[node] == 3:
+        elif G.nodes[node]['type'] == 'FlatCrossing' and G.degree[node] == 3:
             G.add_node(add_limit_node_number, type="LimitOfNetwork")
             G.add_edge(node, add_limit_node_number)
             add_limit_node_number += 1
